6962bcc4bc6baaa31b32d12718635385fcc04d35
Justin <justinlmillan@gmail.com>
2017-01-13 00:29:21 +0000

    first try

---
+++ frank.rb
@@ -0,0 +1,17 @@
+# to run use a command like: ruby frank.rb -p $PORT -o $IP
+
+require 'sinatra'
+
+get '/' do
+    
+    @title = 'Homage to Frank!'
+    
+    erb :index
+end
+
+get '/dos' do 
+    
+    @title = 'Heroku'
+    
+    erb :heroku
+end 
